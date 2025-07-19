238e93f62692a6558d0f4bed63219849c8f7336f
Eyuel M. Gedlie <eyuel.mazengia@aastustudent.edu.et>
2025-07-19 06:54:17 +0300

    2nd commi

---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,66 @@
+import PostCard from "@/components/PostCard";
+import PostModal from "@/components/common/PostModal";
+import Header from "@/components/layout/Header";
+import { PostData, PostProps } from "@/interface";
+import { useState } from "react";
+
+interface PostsPageProps {
+  posts: PostProps[];
+}
+
+const Posts: React.FC<PostsPageProps> = ({ posts }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleAddPost = (newPost: PostData) => {
+    // You may want to actually update the posts array here, but for now just a placeholder
+    // Example: setPosts([...posts, { ...newPost, id: posts.length + 1 }]);
+  };
+
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className=" text-2xl font-semibold">Post Content</h1>
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white"
+          >
+            Add Post
+          </button>
+        </div>
+        <div className="grid grid-cols-3 gap-2 ">
+          {posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
+            <PostCard
+              title={title}
+              body={body}
+              userId={userId}
+              id={id}
+              key={key}
+            />
+          ))}
+        </div>
+      </main>
+
+      {isModalOpen && (
+        <PostModal
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleAddPost}
+        />
+      )}
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Posts;
